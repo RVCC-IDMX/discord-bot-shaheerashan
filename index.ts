453e8af0adc68c4c608e59c8ea7a1ce71a7a5c1d
shaheerashan <77646573+shaheerashan@users.noreply.github.com>
2021-11-07 18:40:04 -0500

    Add parsing

--- index.ts
+++ index.ts
@@ -3,9 +3,7 @@ import DiscordJS, { Intents } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-let prefix = process.env.PREFIX;
-
-let client = new DiscordJS.Client({
+const client = new DiscordJS.Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
@@ -14,48 +12,43 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', (message) => {
-  if (message.content.substring(0, 3) === prefix) {
-    let input = message.content;
-    let command = input.slice(3).trim();
-    let args = command.split(' ');
-    if (args[1] === 'ping') {
-      message
-        .react('🐿️')
-        .then(() => console.log(`Reacted to message "${message.content}"`))
-        .catch(console.error);
-      message
-        .reply({
-          content: 'pong',
-        })
-        .then(() => console.log(`Replied to message "${message.content}"`))
-        .catch(console.error);
-    }
+  if (message.content === 'ping') {
+    message
+      .react('🐿️')
+      .then(() => console.log(`Reacted to message "${message.content}"`))
+      .catch(console.error);
+    message
+      .reply({
+        content: 'pong',
+      })
+      .then(() => console.log(`Replied to message "${message.content}"`))
+      .catch(console.error);
+  }
 
-    let output = cowsay();
+  const output = cowsay();
 
-    if (args[1] === 'cowsay') {
-      message
-        .react('🐱')
-        .then(() => console.log(`Reacted to message "${message.content}"`))
-        .catch(console.error);
-      message
-        .reply({
-          content: `
+  if (message.content === 'cowsay') {
+    message
+      .react('🐱')
+      .then(() => console.log(`Reacted to message "${message.content}"`))
+      .catch(console.error);
+    message
+      .reply({
+        content: `
     \`\`\`
     ${output}
     \`\`\`
       `,
-        })
-        .then(() => console.log(`Replied to message "${message.content}"`))
-        .catch((error) => {
-          if (error.code == 50035) {
-            message
-              .reply(`I did a oopsie...`)
-              .then(() => console.log('Picture too epic'))
-              .catch(console.error);
-          }
-        });
-    }
+      })
+      .then(() => console.log(`Replied to message "${message.content}"`))
+      .catch((error) => {
+        if (error.code == 50035) {
+          message
+            .reply(`I did a oopsie...`)
+            .then(() => console.log('Picture too epic'))
+            .catch(console.error);
+        }
+      });
   }
 });
 
