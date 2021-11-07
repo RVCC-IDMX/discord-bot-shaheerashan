79aab97d54ba1bf12db3c9c23e9edb08a75e9fdb
shaheerashan <77646573+shaheerashan@users.noreply.github.com>
2021-11-07 17:17:17 -0500

    Add random image

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Discord Bot created to display
 [Cowsay Link](https://github.com/piuccio/cowsay)
 
 Cat Avatar obtained from [Vecteezy](https://www.vecteezy.com/vector-art/3341952-illustration-cartoon-litlle-animal-cat-wearing-mask) with a free license with given credit to author.
+
+All sources used by Shaheer Ashan to create a Discord Bot
--- utils/cowsay.ts
+++ utils/cowsay.ts
@@ -1,10 +1,15 @@
 import * as cowsay from 'cowsay';
 import { IOptions } from 'cowsay';
 import getRandomInt from './random';
+import quotes from './quotes.json';
 
 export default function () {
+  // create a random number, also can use the length og quotes
+  const random = getRandomInt(0, quotes.length);
+
+  // To insert a random quote, utilize template literals.
   let opts: IOptions = {
-    text: 'Good Luck People!',
+    text: `${quotes[random].quote} - ${quotes[random].author}`,
     r: true,
   };
 
