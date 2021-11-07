5de1c080222afd93010a985e53a4967ea7c3aa8e
shaheerashan <77646573+shaheerashan@users.noreply.github.com>
2021-11-06 20:30:34 -0400

    Create cowsay
    Successfully created a cowsay message

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import DiscordJS, { Intents } from 'discord.js';
 import dotenv from 'dotenv';
+import * as cowsay from 'cowsay';
 dotenv.config();
 
 const client = new DiscordJS.Client({
@@ -12,9 +13,35 @@ client.on('ready', () => {
 
 client.on('messageCreate', (message) => {
   if (message.content === 'ping') {
-    message.reply({
-      content: 'pong',
-    });
+    message
+      .react('🐱')
+      .then(() => console.log(`Reacted to message "${message.content}"`))
+      .catch(console.error);
+    message
+      .reply({
+        content: 'pong',
+      })
+      .then(() => console.log(`Replied to message "${message.content}"`))
+      .catch(console.error);
+  }
+
+  if (message.content === 'cowsay') {
+    message
+      .react('🐿️')
+      .then(() => console.log(`Reacted to message "${message.content}"`))
+      .catch(console.error);
+
+    let output: string = cowsay.say({ text: 'Lets Do This!' });
+    message
+      .reply({
+        content: `
+    \`\`\`
+    ${output}
+    \`\`\`
+      `,
+      })
+      .then(() => console.log(`Replied to message "${message.content}"`))
+      .catch(console.error);
   }
 });
 
